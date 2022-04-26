79fb8cc567de68bc70de0e590397351f9c43e165
mcsepro <aremsony2k4@yahoo.com>
2022-04-26 15:48:45 +0100

    fixed

--- src/api/middleware/auth.js
+++ src/api/middleware/auth.js
@@ -1,15 +1,15 @@
 const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const Product = require('../models/product')
 const auth = async(req, res, next) => {
 try {
   const token = req.header('Authorization').replace('Bearer ', '')
   const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  const user = await User.findOne({ _id: decoded._id, 'tokens.token':token })
-if(!user) {
+  const product = await User.findOne({ _id: decoded._id, 'tokens.token':token })
+if(!product) {
 throw new Error
 }
   req.token = token
-  req.user = user
+  req.product = product
 next()
 } catch (error) {
 res.status(401).send({error: "Authentication required"})
